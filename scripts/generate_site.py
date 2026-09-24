e0e020443bed1cc6a8aa3b31f6a4334ea963066b
hlavsa13 <98460064+hlavsa13@users.noreply.github.com>
2026-09-24 16:39:20 +0200

    Update generate_site.py

--- scripts/generate_site.py
+++ scripts/generate_site.py
@@ -1,1 +1,29 @@
+import json
+from pathlib import Path
 
+content = Path("content")
+generated = Path("generated")
+
+generated.mkdir(exist_ok=True)
+
+with open(content / "homepage.json", encoding="utf-8") as f:
+    homepage = json.load(f)
+
+html = f"""
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>{homepage["nazev"]}</title>
+</head>
+<body>
+<h1>{homepage["nazev"]}</h1>
+<p>{homepage["obec"]}</p>
+</body>
+</html>
+"""
+
+with open(generated / "index.html", "w", encoding="utf-8") as f:
+    f.write(html)
+
+print("Web vygenerován")
