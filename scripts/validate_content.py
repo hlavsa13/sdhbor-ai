b46b882ab7f16cce27bc7904f1f4479f74f94c94
hlavsa13 <98460064+hlavsa13@users.noreply.github.com>
2026-09-24 16:53:08 +0200

    Update validate_content.py

--- scripts/validate_content.py
+++ scripts/validate_content.py
@@ -1,1 +1,21 @@
+import json
+from pathlib import Path
 
+content = Path("content")
+
+files = [
+    "homepage.json",
+    "historie.json",
+    "souteze.json",
+    "akce.json",
+    "clenove.json",
+    "vedeni.json"
+]
+
+for file in files:
+    path = content / file
+
+    with open(path, encoding="utf-8") as f:
+        json.load(f)
+
+print("Validace OK")
